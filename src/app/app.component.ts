fb0d7690560a6ba60707122184fcda925e9d7232
Kettengeschwindigkeit <666damned999@mail.ru>
2023-10-16 21:11:41 +0300

    full project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { IProduct } from './models/product';
+import { ProductService } from './services/products.service';
+import { ModalService } from './services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular_14_crash_course_2022';
+
 }
